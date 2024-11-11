4e7a30c0eee15563dfd7412099012558fa70f176
Jubayerahmaad <jubuofficial84@gmail.com>
2024-11-12 05:06:54 +0600

    Hader section

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['*'],
+  theme: {
+    extend: {},
+    fontFamily: {
+      'sans': ['Public Sans', 'sans serif'],
+    }
+  },
+  plugins: [],
+}
+
