972e386afb526c25c36a2da3c9b524ed11c11175
Jubayerahmaad <jubuofficial84@gmail.com>
2024-11-15 02:21:19 +0600

    Customer & Review Section

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,19 @@
 module.exports = {
   content: ['*'],
   theme: {
-    extend: {},
+    extend: {
+      container: {
+        center: true, // Centers the container by default
+        padding: '1rem', // Sets default padding for all screens
+        screens: {
+          sm: '600px',
+          md: '700px',
+          lg: '900px',
+          xl: '1200px',
+          '2xl': '1240px', // Custom size for larger screens
+        },
+      },
+    },
     fontFamily: {
       'sans': ['Public Sans', 'sans serif'],
     },
